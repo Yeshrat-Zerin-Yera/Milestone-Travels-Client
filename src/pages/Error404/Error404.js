bd4f842d4d530ed1d9f5e2ba6fc11d06c53846e9
unknown <yeshratzerinyera@gmail.com>
2022-12-18 20:30:48 +0600

    Error Page Added

--- src/pages/Error404/Error404.js
+++ src/pages/Error404/Error404.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Error404 = () => {
     return (
-        <div>
-            Error404
+        <div className='h-[80vh] flex flex-col items-center justify-center'>
+            <h1 className='text-4xl font-bold'>😢 Oops!</h1>
+            <p className='my-6'>Sorry, an unexpected error has occoured.</p>
+            <i>Not Found</i>
         </div>
     );
 };
