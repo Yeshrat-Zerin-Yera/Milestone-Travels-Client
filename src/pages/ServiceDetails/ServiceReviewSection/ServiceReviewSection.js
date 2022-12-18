f16d76d8099392cdfc4637d8abb540ab17e7003a
unknown <yeshratzerinyera@gmail.com>
2022-12-18 21:43:50 +0600

    Safety Commit

--- src/pages/ServiceDetails/ServiceReviewSection/ServiceReviewSection.js
+++ src/pages/ServiceDetails/ServiceReviewSection/ServiceReviewSection.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ServiceReviewSectionForm from '../ServiceReviewSectionForm/ServiceReviewSectionForm';
+import ServiceReviewSectionReviews from '../ServiceReviewSectionReviews/ServiceReviewSectionReviews';
 
 const ServiceReviewSection = ({ service }) => {
 
     return (
         <div>
             <ServiceReviewSectionForm service={service}></ServiceReviewSectionForm>
+            <ServiceReviewSectionReviews service={service}></ServiceReviewSectionReviews>
 
         </div>
     );
--- src/pages/ServiceDetails/ServiceReviewSectionReviews/ServiceReviewSectionReviews.js
+++ src/pages/ServiceDetails/ServiceReviewSectionReviews/ServiceReviewSectionReviews.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ServiceRating from '../../Services/ServiceRating/ServiceRating';
 
-const ServiceReviewSectionReviews = () => {
+const ServiceReviewSectionReviews = ({ service }) => {
+    const [reviews, setReviews] = useState([]);
+    // const { userImg, userName, userEmail, reviewMessage, rating } = reviews;
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/reviews?serviceId=${service?._id}`)
+            .then(res => res.json())
+            .then(data => setReviews(data))
+    }, []);
+    console.log(reviews);
 
     return (
-        <div>
-
+        <div className='lg:w-1/2 md:mr-6 mb-6'>
+            {
+                reviews.map(review => <div key={review?._id} className='m-3 border p-6 rounded-lg'>
+                    <div className='flex items-center gap-3 mb-3'>
+                        <img src={review.userImg} alt="" className='rounded-full w-12 h-12' />
+                        <h2 className='text-2xl'>{review.userName}</h2>
+                    </div>
+                    <p>{review.userEmail}</p>
+                    {
+                        review?.reviewMessage ? <p className='my-3'>{review.reviewMessage}</p> : <p className='my-3 text-slate-700'><i>No Message</i></p>
+                    }
+                    {/* <ServiceRating value={rating}></ServiceRating> */}
+                </div>)
+            }
         </div>
     );
 };
