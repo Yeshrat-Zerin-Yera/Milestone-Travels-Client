961a979ae275e3daebd8492fcb037f9cb8feb440
unknown <yeshratzerinyera@gmail.com>
2022-12-18 22:24:17 +0600

    Safety Commit

--- src/pages/ServiceDetails/ServiceReviewSectionReviews/ServiceReviewSectionReviews.js
+++ src/pages/ServiceDetails/ServiceReviewSectionReviews/ServiceReviewSectionReviews.js
@@ -1,21 +1,10 @@
-import React, { useEffect, useState } from 'react';
 import ServiceRating from '../../Services/ServiceRating/ServiceRating';
 
-const ServiceReviewSectionReviews = ({ service }) => {
-    const [reviews, setReviews] = useState([]);
-    // const { userImg, userName, userEmail, reviewMessage, rating } = reviews;
-
-    useEffect(() => {
-        fetch(`http://localhost:5000/reviews?serviceId=${service?._id}`)
-            .then(res => res.json())
-            .then(data => setReviews(data))
-    }, []);
-    console.log(reviews);
-
+const ServiceReviewSectionReviews = ({ reviews }) => {
     return (
         <div className='lg:w-1/2 md:mr-6 mb-6'>
             {
-                reviews.map(review => <div key={review?._id} className='m-3 border p-6 rounded-lg'>
+                reviews.map(review => <div key={Math.random(100000000000)} className='m-3 border p-6 rounded-lg'>
                     <div className='flex items-center gap-3 mb-3'>
                         <img src={review.userImg} alt="" className='rounded-full w-12 h-12' />
                         <h2 className='text-2xl'>{review.userName}</h2>
